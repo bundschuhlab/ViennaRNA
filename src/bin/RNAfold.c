8ec672b68d635e8c7ac1cf04e53bde60d1d59fba
wampler77 <wampler.77@osu.edu>
2026-07-05 15:13:04 -0400

    Add support for external force option in RNAfold command line

--- src/bin/RNAfold.c
+++ src/bin/RNAfold.c
@@ -48,7 +48,6 @@
 #include "ViennaRNA/datastructures/stream_output.h"
 #include "ViennaRNA/combinatorics.h"
 #include "ViennaRNA/color_output.inc"
-#include "ViennaRNA/ugly_bt.h"
 
 #include "RNAfold_cmdl.h"
 #include "gengetopt_helper.h"
@@ -330,7 +329,7 @@ collect_unnamed_options(struct RNAfold_args_info  *ggostruct,
   *num_files = 0;
 
   /* collect all unnamed options */
-  if ((ggostruct->inputs_num > 0) && (!sanitize_input(ggostruct->inputs[0]))) {
+  if (ggostruct->inputs_num > 0) {
     input_files = (char **)vrna_realloc(input_files, sizeof(char *) * ggostruct->inputs_num);
     for (i = 0; i < ggostruct->inputs_num; i++)
       input_files[(*num_files)++] = strdup(ggostruct->inputs[i]);
